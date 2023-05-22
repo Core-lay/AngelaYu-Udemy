6ec7fab5d2102c4f0e3e0cc1511bbc6208fe73b4
Chris Curley <118066408+Core-lay@users.noreply.github.com>
2023-05-22 11:34:59 +0100

    Add files via upload

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var diceImageView1: UIImageView!
+    
+    @IBOutlet var diceImageView2: UIImageView!
+    
+    let diceArray = [
+    
+    UIImage(named: "DiceOne")!,
+    UIImage(named: "DiceTwo")!,
+    UIImage(named: "DiceThree")!,
+    UIImage(named: "DiceFour")!,
+    UIImage(named: "DiceFive")!,
+    UIImage(named: "DiceSix")!,
+    ]
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+    @IBAction func rollDicePressed(_ sender: UIButton) {
+         
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        
+        
+        
+        
+        
+            }
+    
+}
+
