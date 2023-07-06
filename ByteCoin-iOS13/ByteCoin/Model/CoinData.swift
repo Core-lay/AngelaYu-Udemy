9ca8f7fb9abeaaabe4b344d2bfea7f008233c436
Chris Curley <118066408+Core-lay@users.noreply.github.com>
2023-07-06 11:03:40 +0100

    Add files via upload

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by mac on 4/7/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinData: Decodable {
+    
+    let rate: Double
+}
